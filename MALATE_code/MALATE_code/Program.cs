4c6c64bc886b6457d5a52362367715c13d71515b
Lawrence <lawsmagnet6@gmail.com>
2024-09-20 21:09:50 +0800

    Commit collab 2

--- MALATE_code/MALATE_code/Program.cs
+++ MALATE_code/MALATE_code/Program.cs
@@ -17,6 +17,37 @@ namespace MALATE_code
                 return Math.Round(CWA);
             }
 
+            char Function2(double a)
+            {
+                char LetterGrade = 'X';
+                if (a <= 59 && a != 0)
+                {
+                    LetterGrade = 'F';
+                }
+                else if (a <= 69 && a != 0)
+                {
+                    LetterGrade = 'D';
+                }
+                else if (a <= 79 && a != 0)
+                {
+                    LetterGrade = 'C';
+                }
+                else if (a <= 89 && a != 0)
+                {
+                    LetterGrade = 'B';
+                }
+                else if (a <= 100 && a != 0)
+                {
+                    LetterGrade = 'A';
+                }
+                else if (a == 0)
+                {
+                    Console.WriteLine("No Recorded Weighted Average.");
+                }
+                return LetterGrade;
+            }
+
+            double ave = 0.0;
             Console.WriteLine("Welcome to Automated Student Grade Calculation!");
             Console.WriteLine();
             Console.Write("Enter Student Name: ");
@@ -35,7 +66,7 @@ namespace MALATE_code
                 Console.WriteLine("[4] ------ Exit App");
                 opt = Convert.ToInt32(Console.ReadLine());
 
-                switch(opt)
+                switch (opt)
                 {
                     case 1:
                         Console.Write("Enter Score in Assignment 1 (Out of 10): ");
@@ -45,8 +76,8 @@ namespace MALATE_code
                         Console.Write("Enter Score in Assignment 3 (Out of 10): ");
                         int ass3 = Convert.ToInt32(Console.ReadLine());
 
-                            double totalAss = ((ass1 + ass2 + ass3));
-                            Console.WriteLine();
+                        double totalAss = ((ass1 + ass2 + ass3));
+                        Console.WriteLine();
 
                         Console.Write("Enter Score in Quiz 1 (Out of 20): ");
                         int quiz1 = Convert.ToInt32(Console.ReadLine());
@@ -55,20 +86,27 @@ namespace MALATE_code
                         Console.Write("Enter Score in Quiz 3 (Out of 20): ");
                         int quiz3 = Convert.ToInt32(Console.ReadLine());
 
-                            double totalQuiz = ((quiz1 + quiz2 + quiz3));
-                            Console.WriteLine();
+                        double totalQuiz = ((quiz1 + quiz2 + quiz3));
+                        Console.WriteLine();
 
                         Console.Write("Enter Score in Exam 1 (Out of 50): ");
                         int exam1 = Convert.ToInt32(Console.ReadLine());
                         Console.Write("Enter Score in Exam 2 (Out of 60): ");
                         int exam2 = Convert.ToInt32(Console.ReadLine());
 
-                            double totalExam = ((exam1 + exam2));
-                            Console.WriteLine();
+                        double totalExam = ((exam1 + exam2));
+                        Console.WriteLine();
 
-                        double ave = CWAFunction(totalAss, totalQuiz, totalExam);
+                        ave = CWAFunction(totalAss, totalQuiz, totalExam);
 
                         Console.WriteLine("Your Calculated Weighted Average is " + ave + "%.");
+                        Console.ReadLine();
+                        break;
+
+                    case 2:
+                        char grade = Function2(ave);
+                        Console.WriteLine();
+                        Console.WriteLine("Your letter grade is: " + grade);
                         Console.ReadLine();
                         break;
                 }

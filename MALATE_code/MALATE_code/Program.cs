9f2fbd1df22e7089cf254268dc98d651a44de495
ag3ntk1wi <jmalate019@gmail.com>
2024-09-20 22:29:24 +0800

    FINAL COMMIT

--- MALATE_code/MALATE_code/Program.cs
+++ MALATE_code/MALATE_code/Program.cs
@@ -17,9 +17,7 @@ namespace MALATE_code
                 return Math.Round(CWA);
             }
 
-<<<<<<< HEAD
-=======
-            char Function2(double a)
+            char ALGFunction(double a)
             {
                 char LetterGrade = 'X';
                 if (a <= 59 && a != 0)
@@ -49,8 +47,35 @@ namespace MALATE_code
                 return LetterGrade;
             }
 
+            void DSRFunction(string n, int a, int b, int c, int d, int e, int f, int g, int h, double i, char j)
+            {
+                Console.WriteLine("Name: " + n);
+                Console.WriteLine("Assignment 1: " + a);
+                Console.WriteLine("Assignment 2: " + b);
+                Console.WriteLine("Assignment 3: " + c);
+                Console.WriteLine("Quiz 1: " + d);
+                Console.WriteLine("Quiz 2: " + e);
+                Console.WriteLine("Quiz 3: " + f);
+                Console.WriteLine("Exam 1: " + g);
+                Console.WriteLine("Exam 2: " + h);
+                Console.WriteLine("Weighted Average: " + i);
+                Console.WriteLine("Letter Grade: " + j);
+
+            }
+            int ass1 = 0;
+            int ass2 = 0;
+            int ass3 = 0;
+            int quiz1 = 0;
+            int quiz2 = 0;
+            int quiz3 = 0;
+            int exam1 = 0;
+            int exam2 = 0;
+            double totalAss = 0.0;
+            double totalQuiz = 0.0;
+            double totalExam = 0.0;
+            char grade = 'Z';
             double ave = 0.0;
->>>>>>> 4c6c64bc886b6457d5a52362367715c13d71515b
+
             Console.WriteLine("Welcome to Automated Student Grade Calculation!");
             Console.WriteLine();
             Console.Write("Enter Student Name: ");
@@ -69,80 +94,71 @@ namespace MALATE_code
                 Console.WriteLine("[4] ------ Exit App");
                 opt = Convert.ToInt32(Console.ReadLine());
 
-<<<<<<< HEAD
-                switch(opt)
-=======
                 switch (opt)
->>>>>>> 4c6c64bc886b6457d5a52362367715c13d71515b
                 {
                     case 1:
                         Console.Write("Enter Score in Assignment 1 (Out of 10): ");
-                        int ass1 = Convert.ToInt32(Console.ReadLine());
+                        ass1 = Convert.ToInt32(Console.ReadLine());
                         Console.Write("Enter Score in Assignment 2 (Out of 10): ");
-                        int ass2 = Convert.ToInt32(Console.ReadLine());
+                        ass2 = Convert.ToInt32(Console.ReadLine());
                         Console.Write("Enter Score in Assignment 3 (Out of 10): ");
-                        int ass3 = Convert.ToInt32(Console.ReadLine());
+                        ass3 = Convert.ToInt32(Console.ReadLine());
 
-<<<<<<< HEAD
-                            double totalAss = ((ass1 + ass2 + ass3));
-                            Console.WriteLine();
-=======
-                        double totalAss = ((ass1 + ass2 + ass3));
+                        totalAss = ((ass1 + ass2 + ass3));
                         Console.WriteLine();
->>>>>>> 4c6c64bc886b6457d5a52362367715c13d71515b
 
                         Console.Write("Enter Score in Quiz 1 (Out of 20): ");
-                        int quiz1 = Convert.ToInt32(Console.ReadLine());
+                        quiz1 = Convert.ToInt32(Console.ReadLine());
                         Console.Write("Enter Score in Quiz 2 (Out of 15): ");
-                        int quiz2 = Convert.ToInt32(Console.ReadLine());
+                        quiz2 = Convert.ToInt32(Console.ReadLine());
                         Console.Write("Enter Score in Quiz 3 (Out of 20): ");
-                        int quiz3 = Convert.ToInt32(Console.ReadLine());
+                        quiz3 = Convert.ToInt32(Console.ReadLine());
 
-<<<<<<< HEAD
-                            double totalQuiz = ((quiz1 + quiz2 + quiz3));
-                            Console.WriteLine();
-=======
-                        double totalQuiz = ((quiz1 + quiz2 + quiz3));
+                        totalQuiz = ((quiz1 + quiz2 + quiz3));
                         Console.WriteLine();
->>>>>>> 4c6c64bc886b6457d5a52362367715c13d71515b
 
                         Console.Write("Enter Score in Exam 1 (Out of 50): ");
-                        int exam1 = Convert.ToInt32(Console.ReadLine());
+                        exam1 = Convert.ToInt32(Console.ReadLine());
                         Console.Write("Enter Score in Exam 2 (Out of 60): ");
-                        int exam2 = Convert.ToInt32(Console.ReadLine());
+                        exam2 = Convert.ToInt32(Console.ReadLine());
 
-<<<<<<< HEAD
-                            double totalExam = ((exam1 + exam2));
-                            Console.WriteLine();
-
-                        double ave = CWAFunction(totalAss, totalQuiz, totalExam);
-=======
-                        double totalExam = ((exam1 + exam2));
+                        totalExam = ((exam1 + exam2));
                         Console.WriteLine();
 
                         ave = CWAFunction(totalAss, totalQuiz, totalExam);
->>>>>>> 4c6c64bc886b6457d5a52362367715c13d71515b
 
                         Console.WriteLine("Your Calculated Weighted Average is " + ave + "%.");
                         Console.ReadLine();
                         break;
-<<<<<<< HEAD
-=======
 
                     case 2:
-                        char grade = Function2(ave);
+                        grade = ALGFunction(ave);
                         Console.WriteLine();
                         Console.WriteLine("Your letter grade is: " + grade);
                         Console.ReadLine();
                         break;
->>>>>>> 4c6c64bc886b6457d5a52362367715c13d71515b
+
+                    case 3:
+                        Console.WriteLine("Displaying Student Report...");
+                        Console.WriteLine();
+
+                        DSRFunction(name, ass1, ass2, ass3, quiz1, quiz2, quiz3, exam1, exam2, ave, grade);
+                        Console.ReadLine();
+                        break;
+
+                    case 4:
+                        Console.WriteLine();
+                        Console.WriteLine("Thank You!");
+                        break;
+
+                    default:
+                        Console.WriteLine();
+                        Console.WriteLine("Invalid Option.");
+                        Console.ReadLine();
+                        break;
                 }
             }
 
         }
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 4c6c64bc886b6457d5a52362367715c13d71515b

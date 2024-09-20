02fa52b1346f6db54f0a28d007aa49e997e4f570
Lawrence <lawsmagnet6@gmail.com>
2024-09-20 21:07:42 +0800

    Collab commit 1

---
+++ MALATE_code/MALATE_code/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MALATE_code
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double CWAFunction(double x, double y, double z)
+            {
+                double CWA = ((x + y + z) / 195) * 100;
+                return Math.Round(CWA);
+            }
+
+            Console.WriteLine("Welcome to Automated Student Grade Calculation!");
+            Console.WriteLine();
+            Console.Write("Enter Student Name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine();
+
+            int opt = 0;
+
+            while (opt != 4)
+            {
+                Console.WriteLine("Please select a function you wish to execute:");
+                Console.WriteLine();
+                Console.WriteLine("[1] ------ Calculate Weighted Average");
+                Console.WriteLine("[2] ------ Assign Letter Grade");
+                Console.WriteLine("[3] ------ Display Student Report");
+                Console.WriteLine("[4] ------ Exit App");
+                opt = Convert.ToInt32(Console.ReadLine());
+
+                switch(opt)
+                {
+                    case 1:
+                        Console.Write("Enter Score in Assignment 1 (Out of 10): ");
+                        int ass1 = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Enter Score in Assignment 2 (Out of 10): ");
+                        int ass2 = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Enter Score in Assignment 3 (Out of 10): ");
+                        int ass3 = Convert.ToInt32(Console.ReadLine());
+
+                            double totalAss = ((ass1 + ass2 + ass3));
+                            Console.WriteLine();
+
+                        Console.Write("Enter Score in Quiz 1 (Out of 20): ");
+                        int quiz1 = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Enter Score in Quiz 2 (Out of 15): ");
+                        int quiz2 = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Enter Score in Quiz 3 (Out of 20): ");
+                        int quiz3 = Convert.ToInt32(Console.ReadLine());
+
+                            double totalQuiz = ((quiz1 + quiz2 + quiz3));
+                            Console.WriteLine();
+
+                        Console.Write("Enter Score in Exam 1 (Out of 50): ");
+                        int exam1 = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Enter Score in Exam 2 (Out of 60): ");
+                        int exam2 = Convert.ToInt32(Console.ReadLine());
+
+                            double totalExam = ((exam1 + exam2));
+                            Console.WriteLine();
+
+                        double ave = CWAFunction(totalAss, totalQuiz, totalExam);
+
+                        Console.WriteLine("Your Calculated Weighted Average is " + ave + "%.");
+                        Console.ReadLine();
+                        break;
+                }
+            }
+
+        }
+    }
+}
